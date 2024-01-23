45d01d08c542a7972668d5c66138991af4d79ef2
정승현 <tmdgusya@naver.com>
2024-01-24 06:37:39 +0900

    practicing stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,33 +8,60 @@ void main() {
         title: const Text('Flutter Tutorial'),
       ),
       body: const Center(
-        child: MyButton(),
+        child: Counter(),
       ),
     ),
   ));
 }
 
-class MyButton extends StatelessWidget {
-  const MyButton({Key? key}) : super(key: key);
+// The widget that displays the count
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({Key? key, required this.count}) : super(key: key);
+
+  final int count;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print("Mybutton was tapped!");
-      },
-      child: Container(
-        height: 50,
-        padding: const EdgeInsets.all(8),
-        margin: const EdgeInsets.symmetric(horizontal: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: Colors.lightGreen[500],
-        ),
-        child: const Center(
-          child: Text('Engage'),
-        ),
-      ),
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  const CounterIncrementor({required this.onPressed, super.key});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: const Text('Increment'));
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CounterIncrementor(onPressed: _increment),
+        const SizedBox(width: 16),
+        CounterDisplay(count: _counter)
+      ],
     );
   }
 }
